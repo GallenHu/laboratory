c5137acd2d907920d35f21428e2d694b970d8ebb
Gallen.Hu <gallenhu@foxmail.com>
2017-12-03 14:12:46 +0000

    Update bakmysql.sh

--- bakmysql.sh
+++ bakmysql.sh
@@ -7,7 +7,7 @@ dbname=hi_note
 
 /usr/bin/mysqldump -u root -p$passwd $dbname | gzip > $backupdir/database$time.sql.gz
 # remove files after 7 days
-find $backupdir -name "name_*.sql.gz" -type f -mtime +7 -exec rm {} ; > /dev/null 2>&1
+find $backupdir -name "database*.sql.gz" -type f -mtime +7 -exec rm {} ; > /dev/null 2>&1
 
 # git
 cd $backupdir
