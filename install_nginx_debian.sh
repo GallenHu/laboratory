4254f6b891fb6e68a7096c808471313149bcf620
Gallen.Hu <gallenhu@foxmail.com>
2019-02-19 16:48:19 +0800

    Update install_nginx_debian.sh

--- install_nginx_debian.sh
+++ install_nginx_debian.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 # 安装最新版本nginx, 适用于
-# debian 7
-# debian 8
+# debian 7,8,9
 
 Close_Apache2()
 {
@@ -18,15 +17,19 @@ Install_Nginx()
     v=`cat /etc/os-release|grep VERSION_ID|cut -d \" -f 2`
 
     if [ "$v" == "7" ]; then
-        echo "deb http://nginx.org/packages/debian/ wheezy nginx" >> /etc/apt/sources.list
-        echo "deb-src http://nginx.org/packages/debian/ wheezy nginx" >> /etc/apt/sources.list
+        echo "deb http://nginx.org/packages/mainline/debian/ wheezy nginx" >> /etc/apt/sources.list
+        echo "deb-src http://nginx.org/packages/mainline/debian/ wheezy nginx" >> /etc/apt/sources.list
+    elif [ "$v" == "8" ]; then
+        echo "deb http://nginx.org/packages/mainline/debian/ jessie nginx" >> /etc/apt/sources.list
+        echo "deb-src http://nginx.org/packages/mainline/debian/ jessie nginx" >> /etc/apt/sources.list
     else
-        echo "deb http://nginx.org/packages/debian/ jessie nginx" >> /etc/apt/sources.list
-        echo "deb-src http://nginx.org/packages/debian/ jessie nginx" >> /etc/apt/sources.list
+        echo "deb http://nginx.org/packages/mainline/debian/ stretch nginx" >> /etc/apt/sources.list
+        echo "deb-src http://nginx.org/packages/mainline/debian/ stretch nginx" >> /etc/apt/sources.list
     fi
 
-    wget http://nginx.org/keys/nginx_signing.key && apt-key add nginx_signing.key && apt-get update && apt-get install nginx -y
+    wget http://nginx.org/keys/nginx_signing.key && apt-key add nginx_signing.key && apt-get update && rm nginx_signing.key && apt-get install nginx -y
 }
+
 Install_Nginx
 
 systemctl status nginx.service
