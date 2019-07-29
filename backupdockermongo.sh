8de657b2ab0c5cc98c82a1c098850cfcf5d5b02e
Gallen.Hu <gallenhu@foxmail.com>
2019-07-29 17:30:01 +0800

    Create backupdockermongo.sh

---
+++ backupdockermongo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+backupdir=/home/data/sql_backup/
+time=` date +%Y%m%d%H%M%S `
+passwd=examplepassword
+dbname=noteapp
+dockername=hinote_mongo
+
+docker exec -it $dockername mongodump -h 127.0.0.1 --port 27017 -u=root -p=$passwd --authenticationDatabase admin -d $dbname -o /tmp
+
+docker exec -it $dockername tar -zcvf /tmp/$dbname$time.tar.gz /tmp/$dbname
+
+docker cp $dockername:/tmp/$dbname$time.tar.gz $backupdir
+
+# remove files after 7 days
+find $backupdir -name "noteapp*.tar.gz" -type f -mtime +7 -exec rm {} \; > /dev/null 2>&1
